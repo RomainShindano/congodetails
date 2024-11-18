9c6f29bcf8a883e3030c784b4b247257d26812e0
Ushindi_I_am <70906016+RomainShindano@users.noreply.github.com>
2024-11-18 10:51:46 +0200

    some changes

--- src/app/layout/websiteLayout.js
+++ src/app/layout/websiteLayout.js
@@ -1,6 +1,9 @@
 
 import NavigationBar from "../components/appbar";
-
+export const metadata = {
+    title: "Congo Details",
+    description: "Explore detailed information about the provinces of Congo, including geographical data and local insights.",
+};
 
 
 export default function WebsiteLayout({ children }) {
